228fb066ea5a9d9ae8dac3f3af596febeb290101
Sara <sarahoubertz@gmail.com>
2022-07-13 13:43:16 +0200

    Added funcionality
    Works now, for the most part. Still need to disable the possibility
    of entering multiple operants one after another.
    Maybe try animate numbers so they float into the screen?

--- javascript/doStuff.js
+++ javascript/doStuff.js
@@ -1,32 +1,150 @@
-//get all buttons:
-let reset = document.getElementById("#AC");
-let plusMinus = document.getElementById("#plusMinus");
-let modulo = document.getElementById("#modulo");
-let div = document.getElementById("#divide");
+const calculator = {//to store values
+    displayResult: '',
+    displayValue: '0',
+    firstOperand: '',
+    waitForSecondOperand: false,//if true, the next number entered will constitute the second operand
+    secondOperand: '',
+    operator: null,
+};
 
-let seven = document.getElementById("#seven");
-let eight = document.getElementById("#eight");
-let nine = document.getElementById("#nine");
-let mult = document.getElementById("#multiply");
+const keys = document.querySelector(".buttons").addEventListener("click", (event) =>{
+    const { target } = event;
+    //extract the target property of the click event
+    /*-> what was clicked
+    target variable is an object that represents the element clicked
+    if not a button, ignor and go on
+    otherwise, the type of button and the value pertaining to it
+    is logged*/
+    if(!target.matches("button")) {
+        return;
+    }
+    if(target.classList.contains("operator")) {
+        console.log(target.textContent);
+        handleOperator(target.textContent);
+        if(target.textContent != "=") {
+            updateDisplay();
+        }
+        return;
+    }
 
-let four = document.getElementById("#four");
-let five = document.getElementById("#five");
-let six = document.getElementById("#six");
-let minus = document.getElementById("#minus"); 
+    if(target.classList.contains("decimal")) {
+        setDigit(target.textContent);
+        updateDisplay();
+        return;
+    }
 
-let one = document.getElementById("#one");
-let two = document.getElementById("#two");
-let three = document.getElementById("#three");
-let plus = document.getElementById("#plus");
+    if(target.classList.contains("comma")) {
+        console.log(target.textContent);
+        inputDot(target.textContent);
+        updateDisplay();
+        return;
+    }
 
-let zero = document.getElementById("#zero");
-let comma = document.getElementById("#comma");
-let equal = document.getElementById("#equals");
+    if(target.classList.contains("clear")) {
+        console.log("clear");
+        clear();
+        return;
+    }
+});
 
-//save
-let numbersClicked = [];
-let operatorClicked;
-//set event listeners
+function modulo(num1, num2) {
+    return num1 % num2;
+}
+
+//clear function
+//should wipe everything, all obj or data structures used to store
+//data, all fields etc.
+function clear() {
+    document.querySelector(".window-result").textContent="";
+    calculator.displayValue = '0';
+    calculator.firstOperand = '';
+    calculator.secondOperand = '';
+    calculator.waitForSecondOperand = false;
+    calculator.operator = null;
+    document.querySelector(".window-currentOperator").textContent = calculator.displayValue;
+}
+
+//set current operation field text
+function setDigit(digit) {
+    const { displayValue, firstOperand, waitForSecondOperand } = calculator;
+    console.log(calculator.waitForSecondOperand);
+    if(waitForSecondOperand) {
+        calculator.secondOperand += digit;
+        calculator.displayValue += digit;
+    }
+    else {
+        calculator.displayValue = displayValue == "0" ? digit : displayValue + digit;
+        //calculator.firstOperand += digit;
+    }
+    console.log(calculator);
+}
+
+function inputDot(dot) {
+
+    if(!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
+function handleOperator(Currentoperator) {
+    const { firstOperand, operator, secondOperand, displayValue} = calculator;
+
+    const inputValue = parseFloat(displayValue);
+    calculator.firstOperand = parseFloat(inputValue);
+    
+    if(firstOperand == '' && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    }
+    else if(operator) {
+        const result = operate(firstOperand, operator, secondOperand);
+        calculator.displayResult = String(result);
+        document.querySelector(".window-result").textContent = calculator.displayResult;
+        calculator.displayValue = calculator.displayResult;
+        calculator.firstOperand = calculator.displayResult;
+        document.querySelector(".window-currentOperator").textContent = calculator.displayResult;
+
+        calculator.operator = null;
+        calculator.secondOperand = '';
+        calculator.waitForSecondOperand = false;
+    }
+    console.log(calculator);
+
+    if(Currentoperator != "=") {
+        calculator.displayValue += Currentoperator;
+        calculator.operator = Currentoperator;
+    }
+    calculator.waitForSecondOperand = true;
+    /*if operator is clicked, firstOperand is
+    converted to float (and rounded). 
+    operator property is set to whatever operator was clicked
+    waitingforsecondoperator is set to true as the first one has
+    been finalized*/
+
+}
+
+
+//takes opeator and two numbers and calls one of the above functions
+//depending on the operator received as parameter
+function operate(num1, operator, num2) {
+    let toBeDisplayed;
+    if(operator == "%"){
+        toBeDisplayed = modulo(num1, num2);
+    }
+    else if(operator == "÷") {
+        toBeDisplayed = divide(num1, num2);
+    }
+    else if(operator == "x") {
+        toBeDisplayed = multiply(num1, num2);
+    }
+    else if(operator == "-") {
+        toBeDisplayed = subtract(num1, num2);
+    }
+    else if(operator == "+"){
+        toBeDisplayed = add(num1, num2);
+    }
+
+    return toBeDisplayed;
+}
 
 //basic caluclator functions version one:
 function add(num1, num2) {
@@ -43,53 +161,16 @@ function multiply(num1, num2) {
 
 function divide(num1, num2){
     if(num2 != 0){
-        return Maht.round(num1 / num2);
+        return Math.round(num1 / num2);
     }
     else{
         alert("Division by zero is not supported");
     }
 }
 
-function modulo(num1, num2) {
-    return num1 % num2;
-}
 
-//takes opeator and two numbers and calls one of the above functions
-//depending on the operator received as parameter
-function operate(num1, operator, num2) {
-    if(operator == "%"){
-        modulo(num1, num2);
-    }
-    else if(operator == "/") {
-        divide(num1, num2);
-    }
-    else if(operator == "*") {
-        multiply(num1, num2);
-    }
-    else if(operator == "-") {
-        subtract(num1, num2);
-    }
-    else if(operator == "+"){
-        add(num1, num2);
-    }
-}
-
-//clear function
-//should wipe everything, all obj or data structures used to store
-//data, all fields etc.
-function clear() {
-    document.querySelector(".window-result").textContent="";
-    document.querySelector(".window-currentOperator").textContent="0";
-}
-
-//set current operation field text
-function setCurrentOperation() {
-
-}
-
-//set output field
-function setResult() {
-
+function updateDisplay(){
+    document.querySelector(".window-currentOperator").textContent = calculator.displayValue;
 }
 
 //ensure that after an operator has been pressed, another cannot
@@ -97,7 +178,6 @@ function setResult() {
 
 //give out result to output field if "=" is pressed
 
-//store operator used, and number before and after
 //store number as soon as one of the operators is pressed
 //store second number the same way but maybe in different obj?
 
@@ -109,4 +189,10 @@ function setResult() {
 
 //round answers to two decimals, or maybe one
 
+function ACSymbol() {
+    if(calculator.displayValue != 0) {
+        document.querySelector("#AC").textContent ="C";
+    }
+}
+ACSymbol();
 clear();
